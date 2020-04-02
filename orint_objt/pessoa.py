da65cbbaa4cc2810e855f92fbc666b7134ef6d05
Pedrugust13 <pedro.a.abrao@hotmail.com>
2020-04-02 19:21:58 -0300

    Criados atributo complexo: filho

--- orint_objt/pessoa.py
+++ orint_objt/pessoa.py
@@ -1,12 +1,14 @@
 class Pessoa:
-    def __init__(self, nome = None, idade=30):
+    def __init__(self, *filhos, nome = None, idade=30):
         self.idade = idade
         self.nome = nome
+        self.filhos =  list(filhos)
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 if __name__ == '__main__':
-    p = Pessoa('Parto')
+    p = Pessoa()
+    b = Pessoa(p, nome='Barto')
     print(Pessoa.cumprimentar(p))
     print(id(p))
     print(p.cumprimentar())
@@ -14,4 +16,8 @@ if __name__ == '__main__':
     p.nome = 'Pedrugust'
     print(p.nome)
     print(p.idade)
+    print(b.filhos)
+    for filho in b.filhos:
+        print(filho.nome)
+
 
