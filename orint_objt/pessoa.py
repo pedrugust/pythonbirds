2b7688c6f0ea0b989e2f3988f99576495d691ccd
Pedrugust13 <pedro.a.abrao@hotmail.com>
2020-04-03 15:31:42 -0300

    Criado metodo e atributo de classe Pessoa

--- orint_objt/pessoa.py
+++ orint_objt/pessoa.py
@@ -1,11 +1,17 @@
 class Pessoa:
+    olhos = 2
     def __init__(self, *filhos, nome=None, idade=30):
         self.idade = idade
         self.nome = nome
         self.filhos =  list(filhos)
-    def cumprimentar(self):
+    def cumprimentar(self):     # função de classe referente a objeto chama-se método
         return f'Olá {id(self)}'
-
+    @staticmethod    # decorator
+    def metodo_estatico():    # função de classe com decorator é um metodo de classe, indepennde do objeto
+        return 50
+    @classmethod    # acesso ao nome e aos atributos de classe
+    def nome_atributos(cls):
+        return f'{cls} - {cls.olhos}'
 if __name__ == '__main__':
     p = Pessoa()
     b = Pessoa(p, nome='Barto')
@@ -21,10 +27,15 @@ if __name__ == '__main__':
         print(filho.nome)
     b.sobrenome = 'Abel'
     print(b.__dict__)
+    b.olhos = 1
     print(p.__dict__)
     del b.filhos
     print(b.__dict__)
-
-
-
-
+    print(p.olhos)
+    Pessoa.olhos = 3
+    print(p.olhos)
+    print(b.olhos)  # como objeto b recebeu um valor próprio para olhos, mesmo mudando o valor do atributo para a classe o seu fica
+    print(Pessoa.olhos)
+    print(Pessoa.metodo_estatico())     # não precisa de um objeto
+    print(p.metodo_estatico())     # mas pode ser referenciado a um objeto
+    print(p.nome_atributos(), Pessoa.nome_atributos())
